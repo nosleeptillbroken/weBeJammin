28aab90c827410d82cb3bf7babc469c3ea949c94
Noah <noahmarckirsh@gmail.com>
2017-01-21 14:49:48 -0500

    All basic trap functionality added

---
+++ GGJ2017_Project/Assets/Scripts/Trap Scripts/Snake Trap/SnakeTrapController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeTrapController : MonoBehaviour {
+
+    [SerializeField] private Transform snakeSpawn;
+    [SerializeField] private GameObject snake;
+
+    public bool isActive = false;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isActive)
+        {
+            InvokeRepeating("SpawnSnakes", 1.0f, 4.0f);
+        }
+        else
+            CancelInvoke();
+    }
+
+    void SpawnSnakes()
+    {
+        Instantiate(snake, snakeSpawn.position, snakeSpawn.rotation);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isActive = true;
+            Debug.Log("snaketrap active");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            isActive = false;
+            Debug.Log("snaketrap inactive");
+        }
+    }
+}
