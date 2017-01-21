c98012b6d0549eb5bd24d6d39110e3320226dc29
SOD-SJH-607-12W\Atazir <webb.cody@hotmail.com>
2017-01-21 11:40:57 -0500

    HUD Framework started

---
+++ GGJ2017_Project/Assets/Scripts/PowerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerControl : MonoBehaviour
+{
+    public int power = 0;
+    int maxPower = 100;
+    
+    private int increase()
+    {
+        power += 1;
+        return power;
+    }
+
+    private int decrease()
+    {
+        power -= 1;
+        return power;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetButton("Fire") && power <= maxPower)
+        {
+            increase();
+        }
+        else
+        {
+            decrease();
+        }
+
+	}
+}
