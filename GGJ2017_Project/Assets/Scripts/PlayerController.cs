420a67120e490a64c59ea8e6051f1ed906f3f976
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2017-01-21 02:23:41 -0500

    Player now moves with keyboard input, as well as controller input

--- GGJ2017_Project/Assets/Scripts/PlayerController.cs
+++ GGJ2017_Project/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,16 @@
 using System.Collections;
 using Rewired;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour {
 
     public int playerId = 0; // The Rewired player id of this character
 
-    public float moveSpeed = 3.0f;
-    public float bulletSpeed = 15.0f;
-    public GameObject bulletPrefab;
+    public float moveSpeed = 0.25f;
 
     private Player player; // The Rewired Player
     private PlayerController cc;
     private Vector3 moveVector;
-    private bool fire;
+    private bool jump;
 
     void Awake() {
         // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
@@ -33,20 +30,16 @@ public class PlayerController : MonoBehaviour {
         // Get the input from the Rewired Player. All controllers that the Player owns will contribute, so it doesn't matter
         // whether the input is coming from a joystick, the keyboard, mouse, or a custom controller.
 
-        moveVector.x = player.GetAxis("Move Horizontal"); // get input by name or action id
-        moveVector.y = player.GetAxis("Move Vertical");
-        fire = player.GetButtonDown("Jump");
+
+        jump = player.GetButtonDown("Jump");
     }
 
     private void ProcessInput() {
-        // Process movement
-        if(moveVector.x != 0.0f || moveVector.y != 0.0f) {
-            cc.Move(moveVector * moveSpeed * Time.deltaTime);
-        }
-
-        // Process fire
-        if(fire) {
-
+        Vector3 Move = new Vector3(player.GetAxis("Strafe"), 0f, player.GetAxis("Move"));
+        GetComponent<Rigidbody>().velocity = Move * moveSpeed;
+        if (jump)
+        {
+            Debug.Log("Jumping");
         }
     }
 }
