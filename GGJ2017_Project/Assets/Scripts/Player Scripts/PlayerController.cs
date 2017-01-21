6bebad908f70f6811570176c78ea7233e1658556
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2017-01-21 05:09:50 -0500

    player has camera movement(clamped rotation as well), basic player movement. Jump function is blocked out.

--- GGJ2017_Project/Assets/Scripts/Player Scripts/PlayerController.cs
+++ GGJ2017_Project/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -9,35 +9,25 @@ public class PlayerController : MonoBehaviour {
     public float moveSpeed = 0.25f;
 
     private Player player; // The Rewired Player
-    private PlayerController cc;
-    private Vector3 moveVector;
     private bool jump;
 
     void Awake() {
         // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
         player = ReInput.players.GetPlayer(playerId);
-
-        // Get the character controller
-        cc = GetComponent<PlayerController>();
     }
 
     void Update () {
-        GetInput();
         ProcessInput();
     }
 
-    private void GetInput() {
-        // Get the input from the Rewired Player. All controllers that the Player owns will contribute, so it doesn't matter
-        // whether the input is coming from a joystick, the keyboard, mouse, or a custom controller.
-
-
-        jump = player.GetButtonDown("Jump");
-    }
+   
 
     private void ProcessInput() {
         Vector3 Move = new Vector3(player.GetAxis("Strafe"), 0f, player.GetAxis("Move"));
-        GetComponent<Rigidbody>().velocity = Move * moveSpeed;
-        if (jump)
+        GetComponent<Rigidbody>().velocity = Move * moveSpeed; //player moves left or right
+
+
+        if (player.GetButtonDown("Jump")) //player jumps
         {
             Debug.Log("Jumping");
         }
