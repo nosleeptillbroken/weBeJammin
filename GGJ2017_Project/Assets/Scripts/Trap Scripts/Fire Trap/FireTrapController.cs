19fa20a8c8f7a32e50d5805e3bbc5ff8ceffe796
Noah <noahmarckirsh@gmail.com>
2017-01-21 01:40:46 -0500

    Added basic Fire Trap

---
+++ GGJ2017_Project/Assets/Scripts/Trap Scripts/Fire Trap/FireTrapController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTrapController : MonoBehaviour {
+
+    [SerializeField] private bool isActive = false;
+    [SerializeField] private ParticleSystem fire;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}   
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("on");
+            fire.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("off");
+            fire.Stop();
+        }
+    }
+}
