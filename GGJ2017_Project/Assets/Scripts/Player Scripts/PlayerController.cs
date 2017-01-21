b04fc721d6880cfc7d2f9d737f0d5b11aaec7538
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2017-01-21 14:42:30 -0500

    player can jump and create waves now.

--- GGJ2017_Project/Assets/Scripts/Player Scripts/PlayerController.cs
+++ GGJ2017_Project/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -16,24 +16,37 @@ public class PlayerController : MonoBehaviour {
         // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
         player = ReInput.players.GetPlayer(playerId);
     }
-
-
-    void Update ()
+    
+    void FixedUpdate ()
     {
         ProcessInput();
     }
-
-
-
-    private void ProcessInput() {
-       Vector3 Move = ((transform.right * player.GetAxis("Strafe")) * moveSpeed) + ((transform.forward * player.GetAxis("Move")) * moveSpeed);
-
-        GetComponent<Rigidbody>().velocity = Move; //player moves left or right
-        transform.Rotate(0f, player.GetAxis("Pan") * xSpeed * Time.deltaTime, 0f);
-
-        if (player.GetButtonDown("Jump")) //player jumps
+    
+    private void ProcessInput()
+    {
+        Vector3 MoveAxis = Vector3.forward + Vector3.right;
+        Vector3 Move = ((transform.right * player.GetAxis("Strafe")) * moveSpeed) + ((transform.forward * player.GetAxis("Move")) * moveSpeed);
+        Move.y = GetComponent<Rigidbody>().velocity.y;
+        
+        if (player.GetButtonDown("Jump") && IsGrounded()) //player jumps
         {
-            //Instantiate(wavePrefab, transform.position, transform.rotation);
+            Move.y = 10.0f;
+
+            //
+            GameObject newWaveObject = Instantiate(wavePrefab, transform.position, transform.rotation) as GameObject;
+            Wave wave = newWaveObject.GetComponent<Wave>();
+            //wave.initialLife = the lifespan you want (in seconds)
+            //wave.life = wave.initialLife;
+            //wave.rateOfExpansion = spread (in units per second)
+            //
         }
+
+        GetComponent<Rigidbody>().velocity = Move;//player moves left or right
+        transform.Rotate(0f, player.GetAxis("Pan") * xSpeed * Time.deltaTime, 0f);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f, LayerMask.GetMask("Terrain"));
     }
 }
