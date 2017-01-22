9b5fc2aae6aacb4595fe536553c96c4920586c22
Jacob <its_thrasher@live.ca>
2017-01-22 13:45:52 -0500

    Add busted Audio scripts

--- GGJ2017_Project/Assets/Scripts/GameAudio.cs
+++ GGJ2017_Project/Assets/Scripts/GameAudio.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class GameAudio : MonoBehaviour {
 
+    
+
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	    
 	}
 }
